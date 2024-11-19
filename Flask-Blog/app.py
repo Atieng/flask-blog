8c1735bd7597420f5d61f750b505c517fa0f7ba2
Karen <146369247+Atieng@users.noreply.github.com>
2024-11-19 07:19:07 -0800

    Remove  the Hello World Code

--- Flask-Blog/app.py
+++ Flask-Blog/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 @app.route("/")
@@ -7,9 +7,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route("/about")
-def about():
-    return "<h1>About Page<h1/>"
+# @app.route("/about")
+# def about():
+#     return "<h1>About Page<h1/>"
 
 if __name__ == '__main__':
     app.run(debug= True)
